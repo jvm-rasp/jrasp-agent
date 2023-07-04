0c2936a26ba66b654025ca441a96f01e7bce2b29
-1day <1605368195@qq.com>
2023-07-04 14:08:52 +0800

    修复宿主机上偶发启动失败问题

--- jrasp-daemon/bin/startup.sh
+++ jrasp-daemon/bin/startup.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
-./jrasp-daemon
+# MAX_RETRIES 表示最大重试次数
+MAX_RETRIES=3
+# RETRY_INTERVAL 表示重试间隔时间
+RETRY_INTERVAL=20
+# RETRY_COUNT 表示当前重试次数
+RETRY_COUNT=0
+
+while true; do
+    ./jrasp-daemon
+    if [ $? -eq 0 ]; then
+        echo "jrasp-daemon started successfully"
+        break
+    else
+        RETRY_COUNT=$((RETRY_COUNT+1))
+        if [ $RETRY_COUNT -eq $MAX_RETRIES ]; then
+            echo "Failed to start jrasp-daemon after $MAX_RETRIES attempts"
+            exit 1
+        fi
+        echo "Failed to start jrasp-daemon, retrying in $RETRY_INTERVAL seconds..."
+        sleep $RETRY_INTERVAL
+    fi
+done

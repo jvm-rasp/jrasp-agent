59c443e0100feea8f58346cc14cf1665cdb369f7
-1day <1605368195@qq.com>
2023-10-11 19:41:46 +0800

    issue 41

--- jrasp-daemon/bin/service.sh
+++ jrasp-daemon/bin/service.sh
@@ -2,12 +2,13 @@
 
 while true
 do
-  process=`ps aux | grep jrasp-daemon | grep -v grep`;
-  if [ "$process" == "" ]; then
-      echo "jrasp-daemon is not running";
-      ./jrasp-daemon;
+  process=$(ps aux | grep jrasp-daemon | grep -v grep)
+  // issue 41
+  if [ -z "$process" ] || [[ $process == *"jrasp-daemon.log"* ]]; then
+    echo "jrasp-daemon is not running"
+    ./jrasp-daemon
   else
-      sleep 10;
-      echo "jrasp-daemon is running";
+    sleep 10
+    echo "jrasp-daemon is running"
   fi
 done

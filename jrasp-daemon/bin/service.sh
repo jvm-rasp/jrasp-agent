a530d5f77888fc4dc3d73959a9b878ed4e1919cd
-1day <1605368195@qq.com>
2023-10-11 19:59:24 +0800

    Update service.sh

--- jrasp-daemon/bin/service.sh
+++ jrasp-daemon/bin/service.sh
@@ -3,7 +3,7 @@
 while true
 do
   process=$(ps aux | grep jrasp-daemon | grep -v grep)
-  // issue 41
+  ## issue 41
   if [ -z "$process" ] || [[ $process == *"jrasp-daemon.log"* ]]; then
     echo "jrasp-daemon is not running"
     ./jrasp-daemon

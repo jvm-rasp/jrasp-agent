3214219c2c06b8312c11052a544384cf52e9cfa6
xl1870011370 <1605368195@qq.com>
2023-10-13 09:05:30 +0800

    增加socket消息发送

--- jrasp-daemon/new_socket/daemon_socket.go
+++ jrasp-daemon/new_socket/daemon_socket.go
@@ -5,9 +5,24 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
+	"jrasp-daemon/defs"
 	"jrasp-daemon/socket"
+	"jrasp-daemon/zlog"
 	"net"
+	"time"
 )
+
+const INFO byte = 0x01
+const UNINSTALL byte = 0x02
+const FLUSH byte = 0x03
+const ERROR byte = 0x04
+const UPDATE byte = 0x05
+const UNLOAD byte = 0x06
+const ACTIVE byte = 0x07
+const FROZEN byte = 0x08
+const CONFIG byte = 0x09
+const SEARCH_SERVER byte = 0x10
+const UPDATE_SERVER byte = 0x11
 
 /**
  * daemon 与agent 通信的socket
@@ -28,8 +43,8 @@ func NewServerSocket(port int) *DaemonSocket {
 	}
 }
 
-func (s *DaemonSocket) Start() {
-	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", s.Port))
+func (d *DaemonSocket) Start() {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", d.Port))
 	if err != nil {
 		fmt.Println("Error listening:", err.Error())
 		return
@@ -43,18 +58,22 @@ func (s *DaemonSocket) Start() {
 			continue
 		}
 		// 处理连接
-		go s.handleAgentConnection(conn)
+		go d.handleAgentConnection(conn)
 	}
 }
 
 // 日志处理
-func (s *DaemonSocket) handleAgentConnection(conn net.Conn) {
+func (d *DaemonSocket) handleAgentConnection(conn net.Conn) {
 	if conn != nil {
 		addr := conn.RemoteAddr()
 		if addr != nil {
-			addr.Network()
+			// 注册agent客户端
+			// TODO 暂时不需要特别大的名称
+			address := addr.String()
+			d.Client[address] = conn
 		}
 	}
+
 	scanner := bufio.NewScanner(conn)
 	// 分割
 	scanner.Split(func(data []byte, atEOF bool) (advance int, token []byte, err error) {
@@ -85,4 +104,75 @@ func HandlerAgentMessage(p *socket.Package) {
 	// java agent 的日志写入缓存队列，准备发往server
 	fmt.Println("回传日志:" + string(p.Body))
 	AgentMessageChan <- string(p.Body)
+}
+
+// SendExit  agent退出卸载
+func (d *DaemonSocket) SendExit() {
+	d.SendGroup("", UNINSTALL)
+}
+
+// SendParameters 更新模块参数
+func (d *DaemonSocket) SendParameters(message string) {
+	d.SendGroup(message, UPDATE)
+}
+
+// SendFlushCommand 刷新模块jar包
+func (d *DaemonSocket) SendFlushCommand(isForceFlush string) {
+	d.SendGroup(isForceFlush, FLUSH)
+}
+
+// UpdateAgentConfig 更新agent全局配置
+func (d *DaemonSocket) UpdateAgentConfig(message string) {
+	d.SendGroup(message, CONFIG)
+}
+
+// SendGroup 广播消息
+func (d *DaemonSocket) SendGroup(message string, t byte) {
+	for _, v := range d.Client {
+		d.SendMessge2Conn(v, message, t)
+	}
+}
+
+// SendMessge2Conn 往指定连接发送消息
+func (d *DaemonSocket) SendMessge2Conn(conn net.Conn, message string, t byte) {
+	pack := &Package{
+		Magic:     MagicBytes,
+		Version:   PROTOCOL_VERSION,
+		Type:      t,
+		BodySize:  int32(len(message)),
+		TimeStamp: time.Now().Unix(),
+		Signature: EmptySignature,
+		Body:      []byte((message)),
+	}
+
+	pack.Pack(conn)
+	scanner := bufio.NewScanner(conn)
+	scanner.Split(func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+		if !atEOF && string(data[0:3]) == string(MagicBytes[:]) {
+			if len(data) > 5 {
+				length := int32(0)
+				binary.Read(bytes.NewReader(data[5:9]), binary.BigEndian, &length)
+				if int(length)+145 <= len(data) {
+					return 145 + int(length), data[:145+int(length)], nil
+				}
+			}
+		}
+		return
+	})
+
+	for scanner.Scan() {
+		scannedPack := new(Package)
+		scannedPack.Unpack(bytes.NewReader(scanner.Bytes()))
+		d.Handler(scannedPack)
+	}
+}
+
+// Handler 处理返回的消息
+func (this *DaemonSocket) Handler(p *Package) {
+	switch p.Type {
+	case ERROR:
+		zlog.Errorf(defs.UPDATE_MODULE_PARAMETERS, "update parameters", "result:%s", string(p.Body))
+	default:
+		zlog.Debugf(defs.UPDATE_MODULE_PARAMETERS, "update parameters", "result:%s", string(p.Body))
+	}
 }

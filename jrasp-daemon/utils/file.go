6e2306229b606964183207af14c3a8860181eb16
-1day <1605368195@qq.com>
2023-10-19 20:07:38 +0800

    Update file.go

--- jrasp-daemon/utils/file.go
+++ jrasp-daemon/utils/file.go
@@ -1,8 +1,10 @@
 package utils
 
 import (
+	"bufio"
 	"crypto/md5"
 	"encoding/hex"
+	"fmt"
 	"io"
 	"os"
 )
@@ -40,4 +42,65 @@ func GetFileMd5(pluginPath string) (string, error) {
 		return "", err
 	}
 	return hex.EncodeToString(h.Sum(nil)), nil
+}
+
+func ReadLines(path string, count int) ([]string, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	var lines []string
+	r := bufio.NewReader(f)
+	i := 0
+	for {
+		if count != 0 && i == count {
+			break
+		}
+		// ReadLine is a low-level line-reading primitive.
+		// Most callers should use ReadBytes('\n') or ReadString('\n') instead or use a Scanner.
+		bytes, _, err := r.ReadLine()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return lines, err
+		}
+		lines = append(lines, string(bytes))
+		i++
+	}
+	return lines, nil
+}
+
+func CopyFile(srcFile, dstFile string, perm os.FileMode) error {
+	srcStat, err := os.Stat(srcFile)
+	if err != nil {
+		return err
+	}
+
+	if !srcStat.Mode().IsRegular() {
+		return fmt.Errorf("src file is not a regular file")
+	}
+
+	srcf, err := os.Open(srcFile)
+	if err != nil {
+		return err
+	}
+	defer srcf.Close()
+
+	dstf, err := os.OpenFile(dstFile, os.O_RDWR|os.O_CREATE|os.O_TRUNC, perm)
+	if err != nil {
+		return err
+	}
+	defer dstf.Close()
+	_, err = io.Copy(dstf, srcf)
+	return err
+}
+
+func CreateDir(path string) error {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return os.MkdirAll(path, os.ModePerm)
+	}
+	return nil
 }

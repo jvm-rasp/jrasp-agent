77e08356cd8304e29bf1862995e2673df7f7d326
-1day <1605368195@qq.com>
2023-10-27 15:41:06 +0800

    Update HeartbeatTask.java

--- jrasp-core/src/main/java/com/jrasp/agent/core/task/HeartbeatTask.java
+++ jrasp-core/src/main/java/com/jrasp/agent/core/task/HeartbeatTask.java
@@ -15,7 +15,7 @@ public class HeartbeatTask extends AbstractRaspTask {
     @Override
     public void run() {
         String result = work();
-        LogUtil.info(result.toString());
+        LogUtil.info(result);
     }
 
     /**
@@ -23,30 +23,16 @@ public class HeartbeatTask extends AbstractRaspTask {
      */
     private static String work() {
         Map<String, Object> collector = Monitor.Factory.collector();
-
         StringBuffer sb = new StringBuffer();
-        sb.append("{");
         Iterator<Map.Entry<String, Object>> iterator = collector.entrySet().iterator();
-        int cnt = 0;
         while (iterator.hasNext()) {
             Map.Entry<String, Object> next = iterator.next();
-            appendJsonField(sb, next.getKey(), next.getValue().toString(), cnt < collector.size() - 1);
-            cnt++;
+            sb.append(next.getKey());
+            sb.append("=");
+            sb.append(next.getValue());
+            sb.append(";");
         }
-        sb.append("}");
         return sb.toString();
-    }
-
-    private static void appendJsonField(StringBuffer sb, String key, String value, boolean hasNext) {
-        sb.append("\"").append(key).append("\": ");
-        sb.append("\"").append(value.replace("\"", "\\\"")).append("\"");
-        if (hasNext) {
-            sb.append(", ");
-        }
-    }
-
-    public static void main(String[] args) {
-        work();
     }
 
 }

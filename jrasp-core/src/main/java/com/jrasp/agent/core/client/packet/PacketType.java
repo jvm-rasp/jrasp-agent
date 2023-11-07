a48d18682b3a3c0d429c21a1f9c06ce1a83135ea
-1day <1605368195@qq.com>
2023-11-07 20:24:46 +0800

    Update PacketType.java

--- jrasp-core/src/main/java/com/jrasp/agent/core/client/packet/PacketType.java
+++ jrasp-core/src/main/java/com/jrasp/agent/core/client/packet/PacketType.java
@@ -9,62 +9,62 @@ public enum PacketType {
     /**
      * 更新agent参数
      */
-    AGENT_CONFIG(0x01),
+    AGENT_CONFIG((byte) 0x01),
 
     /**
      * 卸载agent
      */
-    AGENT_UNINSTALL(0x02),
+    AGENT_UNINSTALL((byte) 0x02),
 
     /**
      * 获取agent信息
      */
-    AGENT_INFO(0x03),
+    AGENT_INFO((byte) 0x03),
 
     /**
      * agent日志
      */
-    AGENT_LOG(0x04),
+    AGENT_LOG((byte) 0x04),
 
     // --------------------- module ---------------------
     /**
      * 卸载module
      */
-    MODULE_UNINSTALL(0x20),
+    MODULE_UNINSTALL((byte) 0x20),
 
     /**
      * 更新module参数
      */
-    MODULE_CONFIG(0x21),
+    MODULE_CONFIG((byte) 0x21),
 
     /**
      * 刷新module
      */
-    MODULE_FLUSH(0x22),
+    MODULE_FLUSH((byte) 0x22),
 
     /**
      * 激活模块
      */
-    MODULE_ACTIVE(0x23),
+    MODULE_ACTIVE((byte) 0x23),
 
     /**
      * 冻结模块
      */
-    MODULE_FROZEN(0x24),
+    MODULE_FROZEN((byte) 0x24),
 
     // --------------------- other ---------------------
     /**
      * 命令的返回
      */
-    COMMAND_RESPONSE(0x30);
+    COMMAND_RESPONSE((byte) 0x30);
 
-    private final int value;
+    private final byte value;
 
-    PacketType(int value) {
+    PacketType(byte value) {
         this.value = value;
     }
 
-    public static PacketType getByType(int type) {
+    public static PacketType getByType(byte type) {
         for (PacketType packetType : values()) {
             if (packetType.getValue() == type) {
                 return packetType;

acd3002ab95ab0a2be534977dd6b8a5aa44c5af2
-1day <1605368195@qq.com>
2023-11-07 20:23:52 +0800

    Update Codec.java

--- jrasp-core/src/main/java/com/jrasp/agent/core/client/packet/Codec.java
+++ jrasp-core/src/main/java/com/jrasp/agent/core/client/packet/Codec.java
@@ -13,8 +13,6 @@ public enum Codec {
      */
     INSTANCE;
 
-    private static final byte[] MAGIC_BYTES = {88, 77, 68};
-
     /**
      * 编码并输出
      *
@@ -22,50 +20,28 @@ public enum Codec {
      * @param packet 消息
      * @throws Exception 异常
      */
+    // |<-type(1byte)->|<----bodysize(4byte or int)---->|<----------------------------------body(bytes)------------------->|
     public void encode(DataOutputStream out, Packet packet) throws Exception {
-        int bodySize = packet.getData().length();
-        PacketType packetType = packet.getType();
-        out.write(MAGIC_BYTES);
-        out.write(packet.getPacketHead().getVersion());
-        out.write(packetType.getValue());
-        out.writeInt(bodySize);
-        out.writeLong(packet.getPacketHead().getTimeStamp());
-        out.write(packet.getPacketHead().getSignature());
         String params = packet.getData();
         if (params != null && params.length() > 0) {
-            out.write(params.getBytes(Constant.DEFAULT_CHARSET));
+            PacketType packetType = packet.getType();
+            out.writeByte(packetType.getValue());
+            byte[] bytes = params.getBytes(Constant.DEFAULT_CHARSET);
+            out.writeInt(bytes.length);
+            out.write(bytes);
         }
     }
 
-    // |<---magic(3byte)--->|<-version(1byte)->|<-type(1byte)->|<----bodysize---->|<----time(8bytes)---->|
-    // |<---sig(128)------------------->|<----------------------------------body------------------->|
+    // |<-type(1byte)->|<----bodysize(4byte or int)---->|<----------------------------------body(bytes)------------------->|
     public Packet decode(DataInputStream inputStream) throws Exception {
-        byte[] magicHead = new byte[3];
-        inputStream.readFully(magicHead);
-        verifyMagicHead(magicHead);
+        Packet packet = new Packet();
 
-        int version = inputStream.read();
-        PacketHead packetHead = new PacketHead();
-        packetHead.setVersion(version);
-        verifyVersion(version);
-
-        int type = inputStream.read();
+        byte type = inputStream.readByte();
+        PacketType packetType = PacketType.getByType(type);
+        packet.setType(packetType);
 
         int bodySize = inputStream.readInt();
-        packetHead.setBodySize(bodySize);
-
-        byte[] timeBytes = new byte[8];
-        inputStream.readFully(timeBytes);
-        packetHead.setTimeStamp(toLong(timeBytes));
-
-        byte[] sig = new byte[128];
-        inputStream.readFully(sig);
-
-        PacketType packetType = PacketType.getByType(type);
-        packetHead.setType(packetType);
-
-        Packet packet = new Packet();
-        packet.setPacketHead(packetHead);
+        packet.setBodySize(bodySize);
 
         byte[] bytes = new byte[bodySize];
         inputStream.readFully(bytes);
@@ -74,24 +50,4 @@ public enum Codec {
         return packet;
     }
 
-    private void verifyVersion(int version) throws Exception {
-        if (version != Constant.PROTOCOL_VERSION) {
-            throw new RuntimeException(String.format("unsupported protocol version:%d, current protocol version:%d", version, Constant.PROTOCOL_VERSION));
-        }
-    }
-
-    private void verifyMagicHead(byte[] input) throws Exception {
-        if (input.length != MAGIC_BYTES.length) {
-            throw new RuntimeException("invalid magic head");
-        }
-        for (int i = 0; i < MAGIC_BYTES.length; i++) {
-            if (MAGIC_BYTES[i] != input[i]) {
-                throw new RuntimeException("invalid magic head");
-            }
-        }
-    }
-
-    private long toLong(byte[] readBuffer) {
-        return (((long) readBuffer[0] << 56) + ((long) (readBuffer[1] & 255) << 48) + ((long) (readBuffer[2] & 255) << 40) + ((long) (readBuffer[3] & 255) << 32) + ((long) (readBuffer[4] & 255) << 24) + ((readBuffer[5] & 255) << 16) + ((readBuffer[6] & 255) << 8) + ((readBuffer[7] & 255)));
-    }
 }
